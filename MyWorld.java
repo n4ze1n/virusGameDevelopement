4e799bef7191a5dc8702fb473fa469501959436b
n4ze1n <81848463+n4ze1n@users.noreply.github.com>
2022-06-11 18:35:21 +0800

    update all from n4ze1n

--- MyWorld.java
+++ MyWorld.java
@@ -2,44 +2,67 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class MyWorld extends World
 {
+    //private int lastPipe = 300;
+    private boolean isLost; 
+    //private Timer pipeTimer = new Timer();
+    private Amir Amir = new Amir();
+    //private Amir hati = new Amir();
+    private int actCount; // the counter field
+    private Actor timeDisplay; // the time display actor
+    private Actor hati;
+    //private static GreenfootImage bgImage = new GreenfootImage("world.png");
+    //private static final int SCROLL_SPEED = 5;
+    //private GreenfootImage scrollingImage;
+    // int scrollPosition = 0;
+        
     public MyWorld()
     {   
-        super(500, 300, 1, false); 
+        super(500, 330, 1, false); 
         int dash = 0;
-        addObject(new Player(), 50, 50);
+        addObject(new Player(), 50, 80);
         
+        timeDisplay = new Score();
+        setTimeText();
+        addObject(timeDisplay, 330, 15);
+        
+        hati = new Amir();
+        Amir.updateScore();
+        addObject(hati, 400, 15);
         GreenfootSound music = new GreenfootSound("bgMusic.mp3");
-        if (music.isPlaying())
-            music.stop();
+        //if (music.isPlaying())
+         //   music.stop();
         
-        music.playLoop();
+       music.playLoop();
         
-        //bgMusic(music);//hNNn
+        //bgMusic(music);
         
-        int randNum;
-        randNum = (int) (30 + Math.random() * (50-30+1));
         
-        for(int i=0; i <= 6; i++){
-            dash = dash + 50;
-            addObject(new Virus(), 480, randNum); amir hensem
             
-        }
+        
     }
     public void act()
     {    
             int virusLocation = 500;
+            int AmirCount = 3;
             
             if(Greenfoot.getRandomNumber(60)<1)
-            virusLocation = 50;
+            virusLocation = 80;
             else if(Greenfoot.getRandomNumber(65)<1)
-            virusLocation = 150;
+            virusLocation = 180;
             else if (Greenfoot.getRandomNumber(60)<1)
-            virusLocation = 250;
+            virusLocation = 280;
             
             if(Greenfoot.getRandomNumber(10)==1)
             addVaccine(virusLocation);
 
             addEnemy(virusLocation);
+            
+            if (!getObjects(Player.class).isEmpty()) // only run timer if player in world
+            {
+                    actCount++;
+                    if (actCount%2 == 0) setTimeText();
+            }
+            
         
     }
     public void addEnemy(int virusLocation)
@@ -47,13 +70,34 @@ public class MyWorld extends World
         int location = virusLocation;
         addObject(new Virus(), 500, location);
     }
-public void addVaccine(int virusLocation)
+    public void addVaccine(int virusLocation)
     {
         int location = virusLocation;
         addObject(new Vaccine(), 600, location);
+    } 
+     public void score()
+    {
+        Amir.minus(1);
+        
+        
     }
-public void bgMusic(GreenfootSound music)
-{
+    private void setTimeText()
+    {
+    timeDisplay.setImage(new GreenfootImage("Score: "+(actCount/2), 30, null, null));
+    }
+
+    /**
+     * Stop the game and show the GameOver Screen
+     */
+    public void lost()
+    {
+        removeObjects(getObjects(null));
+        isLost = true;
+        addObject(new GameOver(actCount), 250, 115);
+    }
+   
+    public void bgMusic(GreenfootSound music)
+    {
         /*int counter;
         music.playLoop();
         if (counter.getValue() >=300 ) 
@@ -68,6 +112,6 @@ public void bgMusic(GreenfootSound music)
         */
         //hannan hensem
     
-}
+    }
 
 }
