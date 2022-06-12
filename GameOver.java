27b2ef4bef5ee8d3805c09ae29e5783f36d4543f
kidFreak <96563692+Freak-kid@users.noreply.github.com>
2022-06-12 15:40:40 +0800

    Update GameOver.java

--- GameOver.java
+++ GameOver.java
@@ -1,51 +1,19 @@
-import greenfoot.*;
-
-
 /**
  * This is the Game Over screen displayed when the uses loses the game. 
  * 
- * @author Achyudh, Shantanu
- * @version 14/08/2015
+ * @
+ * @
  */
 public class GameOver extends Actor
-{        
-    public GameOver(int score)
+{   
+    public GameOver()
     {
-        GreenfootImage img = new GreenfootImage("GAME-OVER.png");
-                
-        /*GreenfootImage text1 = new GreenfootImage("Press ENTER to try again!", 30, Color.WHITE, new Color(0, 0, 0, 0));
-        GreenfootImage text2 = new GreenfootImage("SCORE: "+score, 60, Color.WHITE, new Color(0, 0, 0, 0));
-        GreenfootImage text3 = new GreenfootImage("BEST: " + getBestScore(score), 40, Color.WHITE, new Color(0, 0, 0, 0));
         
-        img.drawImage(text1, 250 - (text1.getWidth() / 2), 540);
-        img.drawImage(text2, 250 - (text2.getWidth() / 2), 350);
-        img.drawImage(text3, 250 - (text3.getWidth() / 2), 420);
-        */
-        setImage(img);
+        setImage(new GreenfootImage("Game Over", 48, Color.WHITE, Color.BLACK));
+        Greenfoot.stop();        
+
     }
-    
-    /**
-     * Returns the best score attained by the player and checks if this is a new highscore.
-     * If yes, then it returns that value and sets it as the high score.
-     */
-    private int getBestScore(int score)
-    {
-        UserInfo usrObject = UserInfo.getMyInfo();
-        if(usrObject != null)
-        {
-            int currentHigh = usrObject.getInt(1);
-                       
-            if(score > currentHigh)
-            {
-                usrObject.setInt(1, score);
-                usrObject.store();
-                return score;
-            }
-            else return currentHigh;
-        }
-        else return 0;
-    }
-    
+
     /**
      * Resets the world upon pressing Enter to start a new game.
      */
