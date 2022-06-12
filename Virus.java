34643ea6a23baf11ddd1ec8399114c316017ae43
kidFreak <96563692+Freak-kid@users.noreply.github.com>
2022-06-12 15:51:31 +0800

    Update Virus.java

--- Virus.java
+++ Virus.java
@@ -1,7 +1,15 @@
-import greenfoot.*;
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-public class Virus extends Actor
+/**
+ * Write a description of class Virus here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Virus extends AllActors
 {
+    boolean stumble = false;
+    
     public Virus(){
         GreenfootImage myImage = getImage();
         int myNewHeight = (int)myImage.getHeight()/7;
@@ -15,6 +23,32 @@ public class Virus extends Actor
     public void act()
     {
         move(-10);
-        
+        hitPlayer();
+    }
+    
+    public void hitPlayer()
+    {
+        Actor Player = getOneIntersectingObject(Player.class);
+        if(Player != null)
+        {
+            World myWorld = getWorld();
+            MyWorld lanes = (MyWorld)myWorld;
+            Heart heart = lanes.getHealth();
+            if(stumble == false)
+            {
+                myWorld.removeObject(this);
+                heart.loseHealth();
+                stumble = true;
+                if(heart.health == 0)
+                {
+                    GameOver gameover = new GameOver();
+                    myWorld.addObject(gameover, 250, 165);
+                    myWorld.removeObject(this);
+                }
+        } 
+        else {
+            stumble = true;
+        }
+        }
     }
 }
